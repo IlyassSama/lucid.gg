6c26e971cbfe504fecd45daed3aa43119f6bc2a6
IlyassSama <98023865+IlyassSama@users.noreply.github.com>
2026-02-04 21:09:50 +0100

    Create kars.lua

---
+++ kars.lua
@@ -0,0 +1,32 @@
+local Macros = {}
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+
+Macros[getgenv().Keys[1]] = function()
+    local Event = LocalPlayer.Backpack.ServerTraits.Input
+    Event:FireServer("space")
+    wait(0.025)
+    Event:FireServer(
+        "truem1",
+        {
+            md = Vector3.new(0, 0, 0),
+            skeyreal = false,
+            skeydown = true,
+            dodgevelo = Vector3.new(0, 0, 0),
+            air = false
+        }
+    )
+    wait(0.025)
+    Event:FireServer(
+        "fire",
+        {
+            air = true,
+            range = "1",
+            ToolName = "Victory"
+        }
+    )
+    wait(0.025)
+    Event:FireServer("spaceoff")
+end
+
+return Macros
