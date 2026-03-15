9d76590b70a7236d9c171eefad732e7d25435ac4
IlyassSama <98023865+IlyassSama@users.noreply.github.com>
2026-03-15 13:47:20 +0000

    Update LockImage size to fill the parent container

--- Library.lua
+++ Library.lua
@@ -7680,7 +7680,7 @@ end
                 local LockImage = Instance.new("ImageLabel")
                 LockImage.BackgroundTransparency = 1
                 LockImage.AnchorPoint = Vector2.new(0.5, 0.5)
-                LockImage.Size = UDim2.new(0.8, 0, 0.75, 0)
+                LockImage.Size = UDim2.new(1, 0, 1, 0)
                 LockImage.Position = UDim2.new(0.5, 0, 0.5, 0)
                 LockImage.Image = "rbxassetid://139246255097727"
                 LockImage.ScaleType = Enum.ScaleType.Fit
