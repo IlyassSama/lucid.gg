3a2a7b1566a3c1de0a4ec0bd9a2a8c0ffd59b025
IlyassSama <98023865+IlyassSama@users.noreply.github.com>
2026-03-15 13:54:02 +0000

    Update LockImage size and image asset ID

--- Library.lua
+++ Library.lua
@@ -7680,9 +7680,9 @@ end
                 local LockImage = Instance.new("ImageLabel")
                 LockImage.BackgroundTransparency = 1
                 LockImage.AnchorPoint = Vector2.new(0.5, 0.5)
-                LockImage.Size = UDim2.new(1, 0, 1, 0)
+                LockImage.Size = UDim2.new(0.8, 0, 0.75, 0)
                 LockImage.Position = UDim2.new(0.5, 0, 0.5, 0)
-                LockImage.Image = "rbxassetid://106718203828255"
+                LockImage.Image = "rbxassetid://118592764253807"
                 LockImage.ScaleType = Enum.ScaleType.Fit
                 LockImage.ZIndex = 51
                 LockImage.Parent = LockOverlay
